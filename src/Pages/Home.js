0659b10b8ec95f33ed10c8f1f6a4b3bf095beccd
Samuel-code00 <samphabz00@gmail.com>
2024-07-08 12:06:54 +0100

    update carousal components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,12 @@ import Header from "../components/Header/Header";
 import Hero from "../components/HERO1/Hero";
 import Footer from "../components/Footer/Footer";
 import Quote from "../components/Service/qoute";
-import Carousals from "../components/carousal/Carousel";
+// import Carousals from "../components/carousal/Carousel";
 export default function Home() {
     return(
         <div>
            <Header/>
-           <Carousals/>
+           {/* <Carousals/> */}
            <Hero/>
            <Quote/>
            <Footer/>
